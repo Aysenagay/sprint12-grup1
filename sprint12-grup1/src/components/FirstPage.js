ff0c970c7482947fd27f9cf6d36833e32cc77ff6
cosmopoliitans <elfkyn52@gmail.com>
2023-05-09 11:00:51 +0300

    FirstPageCss

--- sprint12-grup1/src/components/FirstPage.js
+++ sprint12-grup1/src/components/FirstPage.js
@@ -2,13 +2,15 @@ import React from "react";
 
 export default function FirstPage() {
   return (
-    <div className="container">
+    <div className="containerFirstPage">
       <div className="twitter">
         <h3>Twitter'da yeni misin?</h3>
         <p>Kişiselleştirilmiş zaman akışını almak için hemen kaydol!</p>
       </div>
-      <button className="girisYap">Giriş Yap</button>
-      <button className="kayitOl">Kayıt Ol</button>
+      <div className="butonlar">
+        <button className="girisYap">Giriş Yap</button>
+        <button className="kayitOl">Kayıt Ol</button>
+      </div>
     </div>
   );
 }
