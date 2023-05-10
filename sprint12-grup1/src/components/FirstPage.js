1cdc7c25f72904a9e2ac033840243576fe490c5c
cosmopoliitans <elfkyn52@gmail.com>
2023-05-10 09:56:12 +0300

    muslumimg

--- sprint12-grup1/src/components/FirstPage.js
+++ sprint12-grup1/src/components/FirstPage.js
@@ -4,9 +4,9 @@ export default function FirstPage() {
   return (
     <div className="containerFirstPage">
       <div className="twitter">
-        <img src=""></img>
-        <h3>Twitter'da yeni misin?</h3>
-        <p>Kişiselleştirilmiş zaman akışını almak için hemen kaydol!</p>
+        <img src="https://images-ext-1.discordapp.net/external/2hY0AempQUpfWTa5nl86F5NT2R6UYlOlf4UXWriRm-I/%3Fq%3Dtbn%3AANd9GcRdrkk_nCz_jDmG8cQJa5FauH8TyQU836j4RDeYngmgbiGNVtpWitDHbTBpXGBeJsvIsKE%26usqp%3DCAU/https/encrypted-tbn0.gstatic.com/images"></img>
+        <h3>Buralarda yeni misin?</h3>
+        <p>En son haberleri ve güncellemeleri kaçırmamak için hemen üye ol!</p>
       </div>
       <div className="butonlar">
         <button className="girisYap">Giriş Yap</button>
