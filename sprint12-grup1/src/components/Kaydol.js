767ae405a73141819116dda3edf84824dd5d6236
Gülşen Satıcı <125642521+gulsensatici@users.noreply.github.com>
2023-05-10 10:14:06 +0300

    son hali

---
+++ sprint12-grup1/src/components/Kaydol.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Kaydol = () => {
+  return (
+    <div>
+        <div className='signup'>
+            <h3>
+                Hesabını Oluştur
+            </h3>
+        </div>
+       
+    </div>
+  )
+}
