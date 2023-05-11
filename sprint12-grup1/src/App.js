a86e5c8c264a971f135bf6f3f5aaab3ca1781a40
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-11 09:59:33 +0300

    app.js

--- sprint12-grup1/src/App.js
+++ sprint12-grup1/src/App.js
@@ -1,16 +1,25 @@
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Kaydol from "./components/Kaydol";
 import GirisYap from "./components/GirisYap";
 import FirstPage from "./components/FirstPage";
 import "./App.css";
+import { Route, Switch, Router } from "react-router
+
 function App() {
   return (
     <div className="App">
-      <FirstPage />
-      <GirisYap />
+      <Router>
+        <Switch>
+          <Route path="/girisyap">
+            <GirisYap />
+          </Route>
+          <Route path="/kayitol">
+            <Kaydol />
+          </Route>
+          <Route path="/">
+            <FirstPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
-export default App;
