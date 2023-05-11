ccf7c0486fc4c27dbd62481a9e3db7eb564e9ff2
Gülşen Satıcı <125642521+gulsensatici@users.noreply.github.com>
2023-05-11 09:32:47 +0300

    son hali

--- sprint12-grup1/src/components/Kaydol.js
+++ sprint12-grup1/src/components/Kaydol.js
@@ -1,36 +1,35 @@
 import React from 'react'
 
- export default const Kaydol = () => {
-  return (
-    <div className='all'>
-        <div className='signup'>
-            <h3>
-                Hesabını Oluştur
-            </h3>
-        </div>
-        <div className='name'>
-        <label>
-              <input
-                type="text"
-                name="name"
-                placeholder="isim"
-             
-              ></input>
-            </label>
-            </div>
-            <div className='name'>
-        <label>
-              <input
-                type="text"
-                name="name"
-                placeholder="telefon"
-             
-              ></input>
-            </label>
-            </div>
-            <div className='button'>
-            <button className='kaydol'>Kaydol!</button>
-            </div>
-    </div>
-  )
+export default function Kaydol() {
+  
+  <div className='all'>
+  <div className='signup'>
+      <h3>
+          Hesabını Oluştur
+      </h3>
+  </div>
+  <div className='name'>
+  <label>
+        <input
+          type="text"
+          name="name"
+          placeholder="isim"
+       
+        ></input>
+      </label>
+      </div>
+      <div className='name'>
+  <label>
+        <input
+          type="text"
+          name="name"
+          placeholder="telefon"
+       
+        ></input>
+      </label>
+      </div>
+      <div className='button'>
+      <button className='kaydol'>Kaydol!</button>
+      </div>
+</div>
 }
